bce6820b3edcc799de154cec892949fc488ccf32
Szilvia Hosser-Cox <szilvia.hosser@gmail.com>
2018-08-29 22:02:48 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -25,6 +25,7 @@ class PostsController < ApplicationController
   
   def update
     @post = Post.find(params[:id])
-    @post.update = 
+    @post.update(title: params[:title], description: params[:description])
+    
   end
 end
